2203c3170cb62a917a316864084766cf8811f8e8
programming <programadorweb898@gmail.com>
2025-07-20 03:41:10 -0300

    redeplay

--- index.js
+++ index.js
@@ -16,13 +16,25 @@ const __dirname=path.dirname(__filename);
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname,"public")));
+
 app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(specs));
 app.use("/api",router);
 app.use("/auth",authRouter);
-app.use((req,res)=>{
-  res.sendFile(path.join(__dirname,"public","notFound.html"))
-})
+//app.use(express.static(path.join(__dirname,"public")));
+app.use((req, res) => {
+  const acceptsHTML = req.headers.accept?.includes("text/html");
+  
+  //if (acceptsHTML) {
+    //res.status(404).sendFile(path.join(__dirname, "public", "notFound.html"));
+ // }
+//  else {
+    res.status(404).json({
+      error: true,
+      message: "Ruta no encontrada",
+      statusCode: 404
+    });
+//  }
+});
 app.use(errorHandler);
 app.listen(PORT,()=>{
   console.log(`Conección establecida en el puerto ${PORT}`)
